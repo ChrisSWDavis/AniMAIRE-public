7679187b16c85c7acf10190eafba40e59d8c83ce
Chris Davis <ChrisSWDavis@gmail.com>
2025-05-13 15:48:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     package_data={"AniMAIRE":[
                                 "anisotropic_MAIRE_engine/data/*.csv"
                                          ]},
-    version='1.3.1',
+    version='1.3.2',
     description='Python library for running the anisotropic version of MAIRE+',
     long_description=long_description,
     long_description_content_type='text/markdown',
